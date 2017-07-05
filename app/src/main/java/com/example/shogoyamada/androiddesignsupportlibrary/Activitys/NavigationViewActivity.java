4a08978f3aa39376590f6de7c58d0aee663321aa
shogo.yamada <superman199323@gmail.com>
2017-07-05 11:07:57 +0900

    タイトルバーにToggleボタンを作成し、スライドメニューを表示できるように実装

--- app/src/main/java/com/example/shogoyamada/androiddesignsupportlibrary/Activitys/NavigationViewActivity.java
+++ app/src/main/java/com/example/shogoyamada/androiddesignsupportlibrary/Activitys/NavigationViewActivity.java
@@ -1,17 +1,63 @@
 package com.example.shogoyamada.androiddesignsupportlibrary.Activitys;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.shogoyamada.androiddesignsupportlibrary.R;
 
 public class NavigationViewActivity extends AppCompatActivity {
+
+
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_view);
 
+
+        //ツールバーを初期化
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //トグルボタンを作成
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        toggle.setDrawerIndicatorEnabled(true); //ドロワーの使うか使わないかのオンオフ
+        drawerLayout.addDrawerListener(toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // ActionBarDrawerToggleにandroid.id.home(up ナビゲーション)を渡す
+        return (toggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // アクティビティ作成後にトグルボタンを同期
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Android端末のステータスに変化があった場合、その内容をトグルボタンに伝える
+        toggle.onConfigurationChanged(newConfig);
     }
 
 }
