92386ec05a04c0d048fc998abd7b84f286222e8a
shogo.yamada <superman199323@gmail.com>
2017-07-05 13:10:44 +0900

    ボタンを押したらエラーメッセージを表示する

--- app/src/main/java/com/example/shogoyamada/androiddesignsupportlibrary/Activitys/TextInputLayoutActivity.java
+++ app/src/main/java/com/example/shogoyamada/androiddesignsupportlibrary/Activitys/TextInputLayoutActivity.java
@@ -1,7 +1,10 @@
 package com.example.shogoyamada.androiddesignsupportlibrary.Activitys;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.shogoyamada.androiddesignsupportlibrary.R;
 
@@ -11,6 +14,24 @@ public class TextInputLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_input_layout);
+
+        Button error = (Button) findViewById(R.id.error_button);
+        error.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showErrorMessage();
+            }
+        });
+    }
+
+    /**
+     * エラーメッセージを表示する
+     */
+    private void showErrorMessage(){
+
+        TextInputLayout textInputLayout = (TextInputLayout)findViewById(R.id.name);
+        textInputLayout.setError("エラーメッセージを表示！！！");
+        textInputLayout.setErrorEnabled(true);
     }
 
 }
